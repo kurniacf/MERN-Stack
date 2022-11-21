b6ea7a39736216804e9242bd2086a8155cf14d4c
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2022-11-21 14:08:05 +0700

    feat: added models & schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workout');
 const router = express.Router();
 
 // GET all workouts
@@ -12,8 +13,17 @@ router.get('/:id', (req, res) => {
 })
 
 // POST new workout
-router.post('/', (req, res) => {
-    res.json({message: 'POST NEW WORKOUT'});
+router.post('/', async (req, res) => {
+    const {title, reps, load} = req.body;
+
+    try {
+        const workout = await Workout.create({
+            title, reps, load
+        })
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 });
 
 // DELETE workout
